29a82fa3f7ce1f4075e9eaf8b14d9dd65e4b9ecc
fifa f <piorus9196@gmail.com>
2023-01-19 20:17:09 +0100

    tailwind config and add tailwind calendar styles to Calendar component

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,10 +3,12 @@ import AddTask from "./Tasks/Add/AddTask";
 import { TasksConnectToStore } from "./Tasks/TasksConnectToStore";
 import { AddTaskConnectToStore } from "./Tasks/Add/AddTaskConnectToStore";
 import Calendar from "./Calendar";
+import Example from "./Tasks/Cal2";
 const Main = () => {
   return (
     <div>
       <Calendar />
+      <Example />
       {/*<h1>Main</h1>*/}
       {/*<AddTaskConnectToStore />*/}
       {/*<TasksConnectToStore />*/}
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./Store";
 import Main from "./components/Main";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
