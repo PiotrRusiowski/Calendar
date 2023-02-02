6a5bf8b02a3c5a276b4eb7e71bc20cff30487f6b
fifa f <piorus9196@gmail.com>
2023-02-02 22:03:35 +0100

    add composeEnhancers to redux

--- src/Store.js
+++ src/Store.js
@@ -2,5 +2,8 @@ import { createStore, applyMiddleware, compose } from "redux";
 import reducer from "./Reducer";
 import { composeWithDevTools } from "redux-devtools-extension";
 import ReduxThunk from "redux-thunk";
-
-export const store = createStore(reducer, applyMiddleware(ReduxThunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+export const store = createStore(
+  reducer,
+  composeEnhancers(applyMiddleware(ReduxThunk))
+);
